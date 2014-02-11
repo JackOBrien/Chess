8d10c78af62dd11565d376c8a44b1d3e51dd3a94
JackOBrien <obrienjo@mail.gvsu.edu>
2014-02-11 13:13:31 -0500

    Finished Knight

--- Chess/src/model/Knight.java
+++ Chess/src/model/Knight.java
@@ -1,18 +1,27 @@
 package model;
 
-/**
- * The Knight in a game of chess
- * 
- */
+/********************************************************************
+ * CIS 350 - 01
+ * Chess
+ *
+ * A Knight in a game of Chess. Moves in an "L" shape.
+ *
+ * @author John O'Brien
+ * @author Louis Marzorati
+ * @author Shane Higgins
+ * @author Caleb Woods
+ * @version Feb 10, 2014
+ *******************************************************************/
 public class Knight extends ChessPiece {
 
-	/**
-	 * Constructs a new Knight object
+
+	/****************************************************************
+	 * Constructor for night
 	 * 
-	 * @param color the player that owns this piece
-	 */
-	protected Knight(Player color) {
-		super(color);
+	 * @param plr owner of the Knight
+	 ***************************************************************/
+	protected Knight(Player plr) {
+		super(plr);
 	}
 
 	@Override
@@ -22,15 +31,25 @@ public class Knight extends ChessPiece {
 
 	@Override
 	public boolean isValidMove(Move move, IChessBoard board) {
-		if (!super.isValidMove(move, board)) {
+		
+		if (!super.isValidMove(move, board))
 			return false;
-		}
+		
+		// Relative valid positions
+		int[] validRow = {1, 2, 2, 1, -1, -2, -2, -1};
+		int[] ValidCol = {-2, -1, 1, 2, 2, 1, -1, -2};
 			
-		// Knight can jump over another chess piece or pieces
-		// Can move two squares either forward, backward, left, or right
-		// and then left or right one square.
+		int fR = move.fromRow, fC = move.fromColumn;
+		int tR = move.toRow, tC = move.toColumn;
+		
+		/* Loops through array of relative valid positions 
+		 * and returns true if it finds a match*/
+		for (int i = 0; i < validRow.length; i++) {
+			if (tR == fR + validRow[i] && tC == fC + ValidCol[i]) {
+				return true;
+			}
+		}
 
-		// TODO
 		return false;
 	}
 
