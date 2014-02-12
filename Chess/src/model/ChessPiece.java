c97c7793f26ac5bc542a4ff4bbb6963ac92afcfd
JackOBrien <obrienjo@mail.gvsu.edu>
2014-02-11 19:23:58 -0500

    Updated isValidMove()

--- Chess/src/model/ChessPiece.java
+++ Chess/src/model/ChessPiece.java
@@ -1,12 +1,19 @@
 package model;
 
-/**
- * Class that represents a chess piece
+/********************************************************************
+ * CIS 350 - 01
+ * Chess
  *
- * @author YOUR NAME(S)
- */
+ * A game piece in a game of chess. 
+ *
+ * @author John O'Brien
+ * @author Louis Marzorati
+ * @author Shane Higgins
+ * @author Caleb Woods
+ * @version Feb 11, 2014
+ *******************************************************************/
 public abstract class ChessPiece implements IChessPiece {
-	
+
 	private Player owner;
 
 	protected ChessPiece(Player color) {
@@ -19,8 +26,35 @@ public abstract class ChessPiece implements IChessPiece {
 
 	public abstract String type();
 
+	@Override
 	public boolean isValidMove(Move move, IChessBoard board) {
-		// TODO
+		int fR = move.fromRow, fC = move.fromColumn;
+		int tR = move.toRow, tC = move.toColumn;
+		
+		IChessPiece from;
+		IChessPiece to;
+		
+		/* Ensures that only moves contained within the bounds
+		 * of the board are valid */
+		try {
+			from = board.pieceAt(fR, fC);
+			to = board.pieceAt(tR, tC);
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+
+		/* Ensures the piece is actually moving */
+		if (fR == tR && fC == tC) { return false; }
+		
+		/* Ensures the piece at the from location is this piece */
+		if (from != this) { return false; }
+		
+		/* Checks that the piece is not trying to move onto one
+		 * of its own pieces */
+		if (to == null || to.player() != owner) {
+			return true;
+		}
+		
 		return false;
 	}
 }
