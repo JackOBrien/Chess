1539e804e3c6e8611408ca756e46b109278ece99
JackOBrien <obrienjo@mail.gvsu.edu>
2014-02-17 22:26:21 -0500

    Major changes to Pawn

--- Chess/src/model/Pawn.java
+++ Chess/src/model/Pawn.java
@@ -1,25 +1,40 @@
 package model;
 
+import java.lang.Math;
 
-/**
- * A pawn in a game of chess
- * 
- * @author YOUR NAME(S)
- */
+/********************************************************************
+ * CIS 350 - 01
+ * Chess
+ *
+ * A Pawn in a game of chess. 
+ *
+ * @author John O'Brien
+ * @author Louis Marzorati
+ * @author Shane Higgins
+ * @author Caleb Woods
+ * @version Feb 16, 2014
+ *******************************************************************/
 public class Pawn extends ChessPiece {
 
 	private int direction;
+	
 	private int startingRow;
+	
+	private int gamePosition;
+	
+	private boolean movedTwice;
 
-	/**
-	 * Constructs a new Pawn object
+	/****************************************************************
+	 * Constructor for Pawn.
 	 * 
-	 * @param color the player that owns this piece.
-	 */
+	 * @param player the Player who owns this piece
+	 ***************************************************************/
 	public Pawn(Player color) {
 		super(color);
-		direction = (this.player() == Player.WHITE ? 1 : -1);
-		startingRow = (this.player() == Player.WHITE ? 1 : 6);
+		direction = (player() == Player.WHITE ? -1 : 1);
+		startingRow = (player() == Player.WHITE ? 6 : 1);
+		gamePosition = 0;
+		movedTwice = false;
 	}
 
 	@Override
@@ -29,17 +44,97 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public boolean isValidMove(Move move, IChessBoard board) {
+		
 		if (!super.isValidMove(move, board)) {
 			return false;
 		}
-
-		// Pawn can only move one square at at time.
-		// In their first move, they can move two squares if they want.
-		// Pawn can only move straight forward.
-		// Pawn can only capture one of the enemy by approaching diagonally
 		
-		// TODO
+		/* Checks for for en Passant move */
+		if (canEnPassant(move, board)) { return true; }
+		
+		int fR = move.fromRow, fC = move.fromColumn;
+		int tR = move.toRow, tC = move.toColumn;
+		
+		/* Ensures the pawn doesn't move backwards */
+		if ((tR - fR) * direction < 0) return false;
+		
+		/* Check if the piece is not attacking */
+		if (fC == tC) {
+
+			/* Ensures the pawn does not attack forward */
+			if (board.pieceAt(tR, tC) != null) { return false; }
+			
+			/* Move is valid if the pawn only moves forward one */
+			if (tR == fR + direction) { return true; }
+			
+			/* Ensures the spot in front of the pawn is clear */
+			if (board.pieceAt(fR + direction, fC) != null) { return false; }
+
+			
+			/* Checks if the piece is still in its starting location */
+			if (fR == startingRow) {
+				
+				/* Checks if the piece is trying to move forward twice */
+				if (tR == fR + direction * 2) {
+					gamePosition = board.getNumMoves();
+					movedTwice = true;
+					return true;
+				}
+			}		
+		} 
+
+		if (board.pieceAt(move.toRow, move.toColumn) != null) {
+			return isAttacking(move, board);
+		}
+		
 		return false;
 	}
 
+	/****************************************************************
+	 * Tells if the pawn's destination is null and it is moving 
+	 * to one of it's two forward corners
+	 * 
+	 * @param m the move being attempted
+	 * @param b the board the move takes place on
+	 * @return true if the piece is attacking, false otehrwise
+	 ***************************************************************/
+	private boolean isAttacking(Move m, IChessBoard b) {
+			int rowDist = m.toRow - m.fromRow;
+			int colDist = Math.abs(m.fromColumn - m.toColumn);
+
+			// Piece must move up one row and over one column
+			return (rowDist == direction && colDist == 1);
+	}
+	
+	private boolean canEnPassant(Move m, IChessBoard b) {
+		int fR = m.fromRow, tC = m.toColumn;
+		
+		/* Ensures the piece is attacking */
+		if (!isAttacking(m, b)) { return false; }
+		
+		IChessPiece attacked = b.pieceAt(fR, tC);
+		Pawn victim = null;
+		
+		/* Checks for a pawn in the proper position */
+		if (attacked.type().equals(type())) {
+			victim = (Pawn) attacked;
+		} else { return false; }
+		
+		/* Ensures this move is taking place immediately after the victim pawn
+		 * moved into the proper position */
+		if (b.getNumMoves() != victim.getNumMoves() + 1) { return false; }
+		
+		/* If the victim pawn moved two squares at once, this move is valid */
+		return victim.movedTwice;
+	}
+	
+	/****************************************************************
+	 * Returns the number of moves there were on the game board when this
+	 * pawn last moved
+	 * 
+	 * @return number of moves when this piece last moved
+	 ***************************************************************/
+	public int getNumMoves() {
+		return gamePosition;
+	}
 }
