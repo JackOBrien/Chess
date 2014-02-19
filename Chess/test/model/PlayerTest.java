2f2373aef5385af84a84f9ffd5d08d3b3f6aa59b
JackOBrien <obrienjo@mail.gvsu.edu>
2014-02-19 15:43:27 -0500

    Added PlayerTest

---
+++ Chess/test/model/PlayerTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import org.junit.Test;
+
+/********************************************************************
+ * CIS 350 - 01
+ * Chess
+ *
+ * Tests the player to help out EclEmma
+ *
+ * @author John O'Brien
+ * @version Feb 19, 2014
+ *******************************************************************/
+public class PlayerTest {
+
+	@Test
+	public void test() {
+		Player.valueOf(Player.WHITE.toString());
+	}
+
+}
